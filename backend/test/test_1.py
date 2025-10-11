2ae5319aed28d8c6ddeb72a6c72314ab3c662809
Vinícius Castro Coutinho <vcc.snf23@uea.edu.br>
2025-10-11 14:09:18 -0400

    Beginning work on encryption functions Also creating some tests

---
+++ backend/test/test_1.py
@@ -0,0 +1,13 @@
+from backend.encryption_utils import *
+
+DESCRIPTION = "Locally cipher and decipher a message using 3DES"
+MESSAGE = "Bom dia filho. Posso revistar a sua casa?"
+
+key = generate_3des_key()
+nonce, ciphertext = encrypt_message_with_3des(MESSAGE, key)
+deciphered_text = decrypt(nonce, ciphertext, key)
+
+assert deciphered_text == MESSAGE, f"Deciphered text '{deciphered_text}' was different than the MESSAGE {MESSAGE}"
+
+print(f"Original message: '{MESSAGE}'\nDeciphered message: '{deciphered_text}'")
+print(f"3DES Key: '{key}'\nNonce: {nonce}")
