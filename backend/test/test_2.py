12a3bb4972c412cb585d928e35a0bae9cc76b6f6
Vinícius Castro Coutinho <vcc.snf23@uea.edu.br>
2025-10-16 00:33:25 -0400

    Started working on the database
    First draft of the database. Includes a model (.pgerd) to be opened with pgAdmin. Also modifid some docker files to allow proper usage of pgAdmin

--- backend/test/test_2.py
+++ backend/test/test_2.py
@@ -16,4 +16,9 @@ shared_key_B = generate_shared_key_from_df(private_key_B, public_key_A)
 
 assert shared_key_A == shared_key_B, "Reached shared secret, but produced different keys"
 
-print(f"Shared key: {shared_key_A}")
+print(f"Public key A (bits): {public_key_A}\nLength in bytes: {len(b'public_key_A')}. String length: {len(str(public_key_A))}")
+print(f"Private key A: {private_key_A}\nLength in bytes: {len(b'private_key_A')}")
+print(f"Public key B: {public_key_B}\nLength in bytes: {len(b'public_key_B')}. String length: {len(str(public_key_B))}")
+print(f"Private key B: {private_key_B}\nLength in bytes: {len(b'private_key_B')}")
+print(f"Shared secret: {sK}\nLength in bytes: {len(b'sK')}")
+print(f"Shared key: {shared_key_A}\nLength in bytes: {len(shared_key_A)}")
