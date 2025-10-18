fb5c0fbc746df01bab4d954faafa14760fa6dc3f
Vinícius Castro Coutinho <vcc.snf23@uea.edu.br>
2025-10-18 11:26:05 -0400

    entrypoint fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="Sistema de Recomendação de Jogos")
+app = FastAPI(title="Zap")
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,4 +15,7 @@ app.add_middleware(
 
 @app.get("/")
 def is_connected():
-    return {"is_connected" : True}
+    return {"is_connected" : 1}
+
+@app.post("/")
+def create_room():
